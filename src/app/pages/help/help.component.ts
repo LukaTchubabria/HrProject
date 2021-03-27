76485643c06ab56681a7c00c55fc56d0e27766a4
UchaBokeria <ucha1bokeria@gmail.com>
2021-03-27 02:52:14 -0700

    some fixes

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-help',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
-  fakearray = ["ratom armushaobs es saiti?", "damasaqmebt?", "guga movida?"];
-  constructor() { }
+  Questions: any;
+  toggles;
+  
+  constructor(private Data: DataService) {
+    this.Questions = this.Data.getHelpInfo();
+    this.toggles = Array(this.Questions.length);
+
+    for (var i = 0; i < this.toggles.length; i++){
+      this.toggles[i] = false;
+    }
+  }
 
   ngOnInit(): void {
   }
